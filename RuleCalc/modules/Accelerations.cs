cb0dc5c901be4c77e608e249a80a19b7c1f0b52a
Łukasz Świercz <lukasz.swiercz@gmail.com>
2020-02-05 14:54:01 +0100

    beta 0.1

--- RuleCalc/modules/Accelerations.cs
+++ RuleCalc/modules/Accelerations.cs
@@ -86,10 +86,10 @@ namespace RuleCalc.modules
                 double lruleVAR = Convert.ToDouble(info.lData);
                 //LTB.Text = info.lData;
                 double bVAR = Convert.ToDouble(info.bData);
-                //DTB.Text = info.dData;
+                double dVAR = Convert.ToDouble(info.dData);
                 double cbVAR = Convert.ToDouble(info.cbData);
-                //TbalTB.Text = info.tbalData;
-                //TscTB.Text = info.tscData;
+                double tBalVAR = Convert.ToDouble(info.tbalData);
+                double tScVAR = Convert.ToDouble(info.tscData);
                 double gmVAR = Convert.ToDouble(info.gmData);
                 double krVAR = Convert.ToDouble(info.krData);
                 
@@ -105,8 +105,20 @@ namespace RuleCalc.modules
                 double fL = 0;
                 double a0 = 0;
                 double fbkVAR = 0;
-                double angleroll = 0;
-                double T_roll = 0;
+                double accHeave = 0;
+                double accPitch = 0;
+                double cgXtbPrim = 0;
+                double accAxENV = 0;
+                double accAyENV = 0;
+                double accAzENV = 0;
+
+
+
+                //double angleroll = 0;
+                //double T_roll = 0;
+                //double pAngle = 0;
+                //double pPeriod = 0;
+
 
 
 
@@ -201,16 +213,124 @@ namespace RuleCalc.modules
                 // Roll angle, in deg and roll period --> 2.1.1
                 //----------------------------------------------
 
-                T_roll = (2.3 * Math.PI * krVAR) / (Math.Sqrt(9.81 * gmVAR));
-                angleroll = (9000 * (1.4 - 0.035 * T_roll) * frfp * fbkVAR) / ((1.15 * bVAR + 55) * Math.PI);
+                double T_roll = (2.3 * Math.PI * krVAR) / (Math.Sqrt(9.81 * gmVAR));
+                double angleroll = (9000 * (1.4 - 0.035 * T_roll) * frfp * fbkVAR) / ((1.15 * bVAR + 55) * Math.PI);
 
                 //--------------------
-                //2.1.2 Pitch motion
+                // 2.1.2 Pitch motion
                 //--------------------
 
+                double[] pAngleArray = { 20, 920 * 1 * Math.Pow(lruleVAR, -0.84) * (1.0 + Math.Pow((2.57 / Math.Sqrt(9.81 * lruleVAR)), 1.2)) };
+                double pAngleMIN = pAngleArray.Min();
 
-                MessageBox.Show(Convert.ToString(angleroll));
+                double[] fTArray = { 0.5, tBalVAR / tScVAR };
+                double fTMAX = fTArray.Max();
 
+                double fT = fTMAX;
+                double alphaDeg = 0.6 * (1 + fT) * lruleVAR;
+                double pPeriod = Math.Sqrt((2 * Math.PI * alphaDeg) / 9.81);
+
+                double[] vertCORDArray = { (dVAR / 4) + (tBalVAR / 2), dVAR / 2 };
+                double vertCORDMIN = vertCORDArray.Min();
+
+                //---------------------------
+                // 2.2.1 Surge acceleration
+                //---------------------------
+
+                double accSurge = 0.2 * (1.6 + (1.5 / Math.Sqrt(9.81 * lruleVAR))) * frfp * a0 * 9.81;
+
+                //--------------------------
+                // 2.2.2 Sway acceleration
+                //--------------------------
+
+                double accSway = 0.3 * (2.25 - (20 / Math.Sqrt(9.81 * lruleVAR))) * frfp * a0 * 9.81;
+
+                //---------------------------
+                // 2.2.3 Heave acceleration
+                //---------------------------
+
+                if(lruleVAR <100)
+                {
+                    accHeave = 0.8 * (1 + 0.03 * V) * (0.72 + (2 * lruleVAR / 700)) * (1.15 - (6.5 / Math.Sqrt(9.81 * lruleVAR))) * frfp * a0 * 9.81;
+                }
+                else if (lruleVAR <= 100 && lruleVAR < 150)
+                {
+                    accHeave = (0.4 + (lruleVAR / 250)) * (1 + 0.03 * V * (3 - (lruleVAR / 50))) * (1.15 - (6.5 / Math.Sqrt(9.81 * lruleVAR))) * frfp * a0 * 9.81;
+                }
+                else if(lruleVAR >= 150)
+                {
+                    accHeave = (1.15 - (6.5 / Math.Sqrt(9.81 * lruleVAR))) * frfp * a0 * 9.81;
+                }
+
+                //--------------------------
+                // 2.2.4 Roll acceleration
+                //--------------------------
+
+                double accRoll = frfp * angleroll * (Math.PI / 180) * Math.Pow(((2 * Math.PI) / T_roll), 2);
+
+
+                double accRollY = accRoll * (Convert.ToDouble(cgBLtb.Text) - vertCORDMIN); // 3.3.2
+                double accRollZ = accRoll * Convert.ToDouble(cgCLtb.Text); // 3.3.3
+
+                //---------------------------
+                // 2.2.5 Pitch acceleration
+                //---------------------------
+
+                if (lruleVAR < 100)
+                {
+                     accPitch = 0.8 * (1 + 0.05 * V) * frfp * (0.72 + (2 * lruleVAR / 700)) * (1.75 - (22 / Math.Sqrt(9.81 * lruleVAR))) * pAngleMIN * (Math.PI / 180) * (Math.Pow((2 * Math.PI / pPeriod), 2));
+                }
+                else if (lruleVAR <= 100 && lruleVAR < 150)
+                {
+                    accPitch = (0.4 + (lruleVAR / 250)) * (1 + 0.05 * V * (3 - (lruleVAR / 50))) * frfp * (1.75 - (22 / Math.Sqrt(9.81 * lruleVAR))) * pAngleMIN * (Math.PI / 180) * (Math.Pow((2 * Math.PI / pPeriod), 2));
+                }
+                else if (lruleVAR >= 150)
+                {
+                    accPitch = frfp * (1.75 - (22 / Math.Sqrt(9.81 * lruleVAR))) * (Math.Pow((2 * Math.PI / pPeriod), 2));
+                }
+
+                double accPitchX = accPitch * (Convert.ToDouble(cgBLtb.Text) - vertCORDMIN);
+
+                //--------------
+                // CG (X') CGx
+                //--------------
+
+                if(lppVAR > lruleVAR)
+                {
+                    cgXtbPrim = Convert.ToDouble(cgXtb.Text) - (lppVAR - lruleVAR);
+                }
+                else
+                {
+                    cgXtbPrim = Convert.ToDouble(cgXtb.Text) + (lruleVAR - lppVAR);
+                }
+
+                double accPitchZ = accPitch * (1.08 * cgXtbPrim - (0.45 * lruleVAR));
+
+
+                //---------------------------------------
+                // 3.3 Envelope accelerations
+                //---------------------------------------
+
+                // 3.3.1 Longitudinal acceleration
+
+                if(lruleVAR < 90)
+                {
+                    fL = 1;
+                }
+                else if(lruleVAR <= 90 && lruleVAR < 150)
+                {
+                    fL = 1.3 - (lruleVAR / 300);
+                }
+                else if (lruleVAR >= 150)
+                {
+                    fL = 0.8;
+                }
+
+                accAxENV = 0.7 * fL * (0.65 + ((2 * Convert.ToDouble(cgBLtb)) / 7 * tScVAR)); // do zrobienia
+
+
+
+                    
 
 
             }
